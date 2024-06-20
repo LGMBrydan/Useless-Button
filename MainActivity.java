581322ba3f4000e404eea42a021b67e2288108cb
LGM <lgmbrydan12@gmail.com>
2024-06-20 14:12:53 -0400

    Delete MainActivity.java

--- MainActivity.java
+++
@@ -1,8 +0,0 @@
-Button crashButton = findViewById(R.id.crashButton);
-crashButton.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        // Intentionally crash the app
-        throw new RuntimeException("This is a crash");
-    }
-});
